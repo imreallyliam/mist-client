d91ef76a42f0c69a902fb669305fc1cfed4b5a85
Liam Ryde <liamryde@gmail.com>
2024-07-29 15:17:18 -0400

    finish auth flow

--- lib/main.dart
+++ lib/main.dart
@@ -8,28 +8,46 @@ import 'package:mist_client/pages/authentication.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: "MIST",
-    home: const AuthenticationPage(),
-    theme: MistClient.theme(),
-  ));
+      title: "MIST",
+      home: const LoginPreloaderPage(),
+      theme: MistClient.theme()));
 }
 
 class MistClient {
   static const String api = "localhost:80";
+  static bool authenticated = false;
   static String username = "";
   static String baseAuth = "";
   static const FlutterSecureStorage storage = FlutterSecureStorage();
 
   static Widget scaffold(Widget page) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('MIST'),
-        ),
-        body: page);
+    return Scaffold(appBar: appBar(), body: page);
+  }
+
+  static ColorScheme scheme() {
+    return ColorScheme.fromSeed(
+        seedColor: const Color.fromRGBO(35, 187, 96, 1),
+        brightness: Brightness.dark);
   }
 
   static ThemeData theme() {
-    return ThemeData.dark(useMaterial3: true).copyWith();
+    return ThemeData.dark(useMaterial3: true).copyWith(
+        colorScheme: scheme(), scaffoldBackgroundColor: scheme().surface);
+  }
+
+  static AppBar appBar() {
+    return AppBar(
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/icon.png',
+              height: AppBar().preferredSize.height - 10,
+            ),
+            const Padding(padding: EdgeInsets.only(right: 10)),
+            const Text("MIST")
+          ],
+        ),
+        centerTitle: true);
   }
 
   static Widget waveDots() {
@@ -46,7 +64,12 @@ class MistClient {
       var response = await get(Uri.http(api, '/api/protected'), headers: {
         'Authorization': baseAuth,
       });
-      return (response.statusCode == 200 && response.body == "Hello, world!");
+      if (response.statusCode == 200 && response.body == "Hello, world!") {
+        authenticated = true;
+        return true;
+      } else {
+        return false;
+      }
     } else {
       return false;
     }
--- lib/pages/authentication.dart
+++ lib/pages/authentication.dart
@@ -1,20 +1,22 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:http/http.dart';
+import 'package:markdown_widget/widget/markdown_block.dart';
 import 'package:mist_client/main.dart';
 import 'package:mist_client/pages/home.dart';
 
-class AuthenticationPage extends StatefulWidget {
-  const AuthenticationPage({super.key});
+class LoginPreloaderPage extends StatefulWidget {
+  const LoginPreloaderPage({super.key});
 
   @override
   State<StatefulWidget> createState() {
-    return AuthenticationPageState();
+    return LoginPreloaderPageState();
   }
 }
 
-class AuthenticationPageState extends State<AuthenticationPage> {
+class LoginPreloaderPageState extends State<LoginPreloaderPage> {
   @override
   void initState() {
     super.initState();
@@ -52,59 +54,100 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Form(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextFormField(
-            controller: _usernameController,
-            decoration: const InputDecoration(labelText: 'Username'),
-          ),
-          TextFormField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: const InputDecoration(labelText: 'Password'),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              var response =
-                  await post(Uri.http(MistClient.api, '/api/authenticate'),
-                      body: jsonEncode({
-                        'username': _usernameController.text,
-                        'password': _passwordController.text,
-                      }));
-              if (response.statusCode == 200) {
-                Map<String, dynamic> body = jsonDecode(response.body);
-                await MistClient.storage
-                    .write(key: "username", value: _usernameController.text);
-                await MistClient.storage
-                    .write(key: "token", value: body['token']);
-                Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const AuthenticationPage()));
-              } else {
-                showDialog(
-                    context: context,
-                    builder: (context) => AlertDialog(
-                          title: const Text('Login Failed'),
-                          content: const Text(
-                              'Please check your username and password.'),
-                          actions: [
-                            TextButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text('OK'))
-                          ],
-                        ));
-              }
-            },
-            child: const Text('Login'),
-          )
-        ],
-      ),
-    ));
+    return Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: StaggeredGrid.count(
+          crossAxisCount: MediaQuery.of(context).size.width > 600 ? 2 : 1,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          children: [
+            const Card(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: MarkdownBlock(data: '''
+## Welcome! 
+  
+Please login with your FCPS credentials to continue.  
+  
+Experiencing issues logging in? Please [email laryde@fcps.edu](mailto:laryde@fcps.edu)
+                '''),
+              ),
+            ),
+            Card(
+              child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Form(
+                    autovalidateMode: AutovalidateMode.always,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          controller: _usernameController,
+                          autofillHints: const [AutofillHints.username],
+                          decoration:
+                              const InputDecoration(labelText: 'Username'),
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          autofillHints: const [AutofillHints.password],
+                          decoration:
+                              const InputDecoration(labelText: 'Password'),
+                          onFieldSubmitted: (str) {
+                            login(context);
+                          },
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.only(top: 10.0),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            login(context);
+                          },
+                          child: const Text('Login'),
+                        )
+                      ],
+                    ),
+                  )),
+            )
+          ],
+        ));
+  }
+
+  void login(BuildContext context) async {
+    var response = await post(Uri.http(MistClient.api, '/api/authenticate'),
+        body: jsonEncode({
+          'username': _usernameController.text,
+          'password': _passwordController.text,
+        }));
+    if (response.statusCode == 200) {
+      Map<String, dynamic> body = jsonDecode(response.body);
+      await MistClient.storage
+          .write(key: "username", value: _usernameController.text);
+      await MistClient.storage.write(key: "token", value: body['token']);
+      if (context.mounted) {
+        Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) => const LoginPreloaderPage()));
+      }
+    } else {
+      if (context.mounted) {
+        showDialog(
+            context: context,
+            builder: (context) => AlertDialog(
+                  title: const Text('Login Failed'),
+                  content:
+                      const Text('Please check your username and password.'),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text('OK'))
+                  ],
+                ));
+      }
+    }
   }
 }
